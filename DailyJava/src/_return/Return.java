d4f6860749c70b17aa0868e93b0ca04581b19b99
gyul <gyul@namoo>
2024-07-29 21:36:37 +0900

    return문의 반환(메소드 실행즉시종료)

--- DailyJava/src/_return/Return.java
+++ DailyJava/src/_return/Return.java
@@ -9,11 +9,16 @@ public class Return {
         } else if(i==2){
             return "two";
         }
-        return "none";
+        return "none";// String으로 반환해주어야함
+        // void가 아닌 메소드는 반드시 값을 반환해야함
+        // return문은 메소드 실행을 즉시 종료한다
+        
     }
  
     public static void main(String[] args) {
         System.out.println(num(1));
+        // one
+        // none
     }
 	
 }
--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -5,14 +5,18 @@ public class Main {
         if(i==0){
             return "zero";
         } else if(i==1){
-            System.out.println("one"); 
+            System.out.println("one1"); 
         } else if(i==2){
-            return "two";
+            return "two";     
         }
-        return "none";
+        return "none";   // String으로 반환해주어야함
+        // void가 아닌 메소드는 반드시 값을 반환해야함
+        // return문은 메소드 실행을 즉시 종료한다
     }
  
     public static void main(String[] args) {
         System.out.println(num(1));
+        // one
+        // 
     }
 }
